8c3afd2fdd03260bf8ef0e0b68f9d5d0f874e84a
Maarten van Gompel <proycon@anaproy.nl>
2013-08-12 21:45:05 +0200

    new wallpaper

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -5,7 +5,8 @@ setinput.sh
 pulseaudio --start &
 gnome-settings-daemon &
 gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg &
-feh --bg-scale ~/Pictures/Local/wallpapers/shirahige2.jpg &
+#feh --bg-scale ~/Pictures/Local/wallpapers/shirahige2.jpg &
+feh --bg-scale ~/Pictures/Local/wallpapers/Perfection_Cold_by_yaromanzarek.jpg &
 setlayout 0 3 3 0
 synapse -s &
 if [[ $HOST == "mhysa" ]]; then
@@ -18,6 +19,7 @@ conky &
 nm-applet &
 udiskie &
 ibus-daemon -d -x
-sleep 3 && feh --bg-scale ~/Pictures/Local/wallpapers/shirahige2.jpg &
+#sleep 3 && feh --bg-scale ~/Pictures/Local/wallpapers/shirahige2.jpg &
+sleep 2 && feh --bg-scale ~/Pictures/Local/wallpapers/Perfection_Cold_by_yaromanzarek.jpg &
 play ~/cylontune_low.ogg &
 
