1c69a89008715495e0df97211d92139e9540075f
Maarten van Gompel <proycon@anaproy.nl>
2014-08-08 23:36:42 +0200

    vim style readline!

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ ln -s $DOTDIR/tm $HOMEDIR/bin/tm
 ln -s $DOTDIR/ncmpcpp.config $HOMEDIR/.ncmpcpp/config
 ln -s $DOTDIR/pdbrc.py $HOMEDIR/.pdbrc.py
 ln -s $DOTDIR/vimperatorrc $HOMEDIR/.vimperatorrc
+ln -s $DOTDIR/inputrc $HOMEDIR/.inputrc
 NICKSERVPASS=`cat ~/.nickserv`
 BOUNCERPASS=`cat ~/.anaproy`
 cat $HOMEDIR/.irssi/config.masked | sed -e "s/NICKSERVPASS/$NICKSERVPASS/" -e "s/BOUNCERPASS/$BOUNCERPASS/" > $HOMEDIR/.irssi/config
