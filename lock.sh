8b5fdc39d32b1497f5a6614abb774326331dc017
Maarten van Gompel <proycon@anaproy.nl>
2015-07-28 12:01:50 +0200

    added

---
+++ lock.sh
@@ -0,0 +1,6 @@
+#scrot -e 'convert -resize 20% -fill "#282828" -colorize 50% -blur 0x1 -resize 500% $f ~/lockbg.png'
+play ~/dotfiles/media/lock.wav >/dev/null 2>/dev/null &!
+scrot -e "convert \$f -fill '#282828' -colorize 50% -scale 10% -scale 1000% ~/lockbg.png" "lockbg.png"
+convert -gravity center -composite ~/lockbg.png ~/dotfiles/media/locked.png ~/lockfinal.png
+i3lock -u -i ~/lockfinal.png
+rm ~/lockfinal.png ~/lockbg.png
