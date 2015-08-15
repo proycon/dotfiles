271f4c9472c65b2abcdf06978dc2c42699dcaf15
Maarten van Gompel (proycon) <proycon@anaproy.nl>
2015-08-15 23:09:06 +0200

    lock feedback

--- lock.sh
+++ lock.sh
@@ -4,6 +4,6 @@ if [[ ! -f /tmp/locked ]]; then
     play ~/dotfiles/media/lock.wav >/dev/null 2>/dev/null &!
     scrot -e "convert \$f -fill '#282828' -colorize 50% -scale 10% -scale 1000% ~/lockbg.png" "lockbg.png"
     convert -gravity center -composite ~/lockbg.png ~/dotfiles/media/locked.png ~/lockfinal.png
-    i3lock -u -i ~/lockfinal.png
+    i3lock -e -b -i ~/lockfinal.png
     rm ~/lockfinal.png ~/lockbg.png /tmp/locked
 fi
