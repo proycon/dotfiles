226da006b0d9e480b7283c7a6f55781055ca3ee6
Maarten van Gompel <proycon@anaproy.nl>
2016-06-19 15:44:39 +0200

    updated lock

--- lock.sh
+++ lock.sh
@@ -2,8 +2,10 @@
 if [[ ! -f /tmp/locked ]]; then
     touch /tmp/locked
     play ~/dotfiles/media/lock.wav >/dev/null 2>/dev/null &!
-    scrot -e "convert \$f -fill '#282828' -colorize 50% -scale 10% -scale 1000% ~/lockbg.png" "lockbg.png"
-    convert -gravity center -composite ~/lockbg.png ~/dotfiles/media/locked.png ~/lockfinal.png
-    i3lock -e -b -i ~/lockfinal.png
-    rm ~/lockfinal.png ~/lockbg.png /tmp/locked
+    #scrot -e "convert \$f -fill '#282828' -colorize 50% -scale 10% -scale 1000% ~/lockbg.png" "lockbg.png"
+    #convert -gravity center -composite ~/lockbg.png ~/dotfiles/media/locked.png ~/lockfinal.png
+    i3lock -c '#000000' -o '#191d0f' -w '#572020' -l '#ffffff' -e --24
+    #i3lock -e -b -i ~/lockfinal.png
+    #rm ~/lockfinal.png ~/lockbg.png /tmp/locked
+    rm /tmp/locked
 fi
