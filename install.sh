388107b40cddb980b0145ca844d5f2b1d1ef237d
Maarten van Gompel <proycon@anaproy.nl>
2019-01-23 22:20:38 +0100

    installation fixes

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ if [ $SUDO -eq 1 ]; then
         sudo apt install rxvt-unicode aptitude tmux compton i3-wm tig neovim i3lock ncdu ranger curl wget jedi rofi dmenu openbox ipython ipython3 pcmanfm gcc mpv autoconf-archive pandoc htop glances iotop netcat newsboat mplayer fcitx firefox pavucontrol ncmpcpp git whiptail zathura zathura-pdf-poppler gimp inkscape ack jq sed
         sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 #for compiling polybar
     elif [[ "$OS" == "arch" ]]; then
-        sudo pacman -Syyu rxvt-unicode tmux compton i3-gaps neovim tig i3lock ncdu ranger curl wget rofi dmenu openbox ipython pcmanfm gcc mpv autoconf-archive pandoc htop glances iotop netcat sxiv newsboat mplayer fcitx firefox pavucontrol ncmpcpp git fzf zathura zathura-pdf-poppler gimp inkscape ack bat jq fd sed
+        sudo pacman -Syyu --needed rxvt-unicode tmux compton i3-gaps neovim tig i3lock ncdu ranger curl wget rofi dmenu openbox ipython pcmanfm gcc mpv autoconf-archive pandoc htop glances iotop netcat sxiv newsboat mplayer fcitx firefox pavucontrol ncmpcpp git fzf zathura zathura-pdf-poppler gimp inkscape ack bat jq fd sed
     else
         echo "Distribution not supported!">&2
         sleep 10
@@ -76,14 +76,18 @@ ROOTNAMES=("vim" "vimrc" "zshrc" "urlview" "muttrc" "urxvt" "gdbinit"  "mailcap"
 
 CONFIGNAMES=("openbox" "bspwm" "sxhkd" "nvim" "ranger" "polybar" "sxiv" "i3" "ipython" "tm" "ncmpcpp.config")
 
-for NAME in $ROOTNAMES; do
+for NAME in ${ROOTNAMES[*]}; do
+    echo "$DOTDIR/$NAME"
     if [ -e "$DOTDIR/$NAME" ]; then
+	echo "Linking $NAME"
         ln -sfn "$DOTDIR/$NAME" "$HOMEDIR/.$NAME"
     fi
 done
 
-for NAME in $CONFIGNAMES; do
+for NAME in ${CONFIGNAMES[*]}; do
+    echo "$DOTDIR/$NAME"
     if [ -e "$DOTDIR/$NAME" ]; then
+	echo "Linking $NAME"
         ln -sfn "$DOTDIR/$NAME" "$HOMEDIR/.config/$NAME"
     fi
 done
