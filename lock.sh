85dfa555dc9f5abcb36774da89f102df7515710c
Maarten van Gompel <proycon@anaproy.nl>
2019-12-19 22:50:36 +0100

    screensaver background + mutt

--- lock.sh
+++ lock.sh
@@ -4,7 +4,7 @@ if [[ ! -f /tmp/locked ]]; then
     play ~/dotfiles/media/lock.wav >/dev/null 2>/dev/null &!
     #scrot -e "convert \$f -fill '#282828' -colorize 50% -scale 10% -scale 1000% ~/lockbg.png" "lockbg.png"
     #convert -gravity center -composite ~/lockbg.png ~/dotfiles/media/locked.png ~/lockfinal.png
-    i3lock -c '#000000'
+    i3lock -c '#220000'
     #i3lock -e -b -i ~/lockfinal.png
     #rm ~/lockfinal.png ~/lockbg.png /tmp/locked
     rm /tmp/locked
