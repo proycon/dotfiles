f35db4bd7abf9911862551635362be005b52d333
Maarten van Gompel <proycon@anaproy.nl>
2020-05-17 00:41:57 +0200

    fixes

--- opensxiv.sh
+++ opensxiv.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-dir=$(ls -d ~/Pictures/*/  | sed "s|$HOME/Pictures/||g" | dmenu -fn Sans -l 20 -i -p "Open Folder?")
-sxiv -rt "$HOME/Pictures/$dir"
+dir=$(ls -d ~/Pictures/*/  | sed "s|$HOME/Pictures/||g" | rofi -dmenu -p "Open Folder?")
+# Exit if none chosen.
+[ -z "$dir" ] && exit
+echo "$dir" | tr -d '\n' | xclip -selection clipboard
+export PATH=$HOME/bin:$PATH
+sxiv -rt "$HOME/Pictures/$dir" 2> /tmp/err
