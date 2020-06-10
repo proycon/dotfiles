687a677483d987f9ff845426e3f46a39d9fc8138
Maarten van Gompel <proycon@anaproy.nl>
2020-06-10 20:05:12 +0200

    adding NO_DESKTOP environment variable for more minimal installation

--- install.sh
+++ install.sh
@@ -53,8 +53,12 @@ done
 if [ $SUDO -eq 1 ]; then
     if [[ "$OS" == "debian" ]]; then
         sudo apt update
-        sudo apt install rxvt-unicode aptitude tmux compton i3-wm tig neovim i3lock ncdu ranger curl wget jedi rofi dmenu openbox ipython ipython3 pcmanfm gcc mpv autoconf-archive pandoc htop glances iotop netcat newsboat mplayer fcitx firefox pavucontrol ncmpcpp git whiptail zathura zathura-pdf-poppler gimp inkscape ack jq sed fping pamixer
-        sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 highlight chafa #for compiling polybar
+        sudo apt install aptitude tmux tig neovim ncdu ranger curl wget jedi ipython ipython3 gcc autoconf-archive pandoc htop glances iotop netcat git whiptail ack jq sed fping highlight chafa
+        if [ ! -z "$NO_DESKTOP" ]; then
+            sudo apt install rxvt-unicode compton rofi dmenu jedi i3-wm i3lock openbox newsboat mpv mplayer fcitx firefox pavucontrol ncmpcpp zathura zathura-pdf-poppler gimp inkscape pamixer
+            sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 #for compiling polybar
+            sudo apt install rustc cargo #for compiling alacritty and other rust stuff
+        fi
     elif [[ "$OS" == "arch" ]]; then
         sudo pacman -Syyu --needed sudo openssh aspell aspell-de aspell-en aspell-es aspell-fr aspell-it aspell-nl aspell-ru audacity autoconf autoconf-archive bat bzip2 calibre cheese chromium cmake compton debootstrap deluge docker doxygen e2fsprogs encfs eslint evince fcitx fcitx-gtk3 fcitx-gtk2 fcitx-qt4 fcitx-qt4 feh ffmpeg firefox gawk gcc gdb geeqie gedit gimp git gitg glances gmp gnome-desktop gnome-common gnupg gnu-netcat gnome-settings-daemon gnome-keyring go gnutls gnuplot grep gpgme gpm groovy graphviz gstreamer gst-plugins-good gst-plugins-base gst-plugins-bad gst-plugins-ugly gzip htop hdparm hunspell i3-gaps icu ipython imagemagick iotop iperf jupyter keybase lm_sensors lsb-release lsof lua make mailcap m4 maven mesa mplayer mpv nano nautilus ncdu mosquitto mpc networkmanager newsboat ncmpcpp nmap nnn ranger okular openssl perl poppler powerline procps-ng psmisc python python-cherrypy python-lxml python-django python-setuptools python-jinja python-flask python-matplotlib python-numpy python-oauth2client python-oauthlib python-jupyter_core python-jupyter_client python-pandas python-pip python-pillow python-psutil python-requests python-seaborn python-setuptools python-virtualenv python2 python2-lxml python2-numpy python2-scipy python-yaml python2-yaml python2-virtualenv riot-desktop riot-web rofi rsync readline ruby rust rxvt-unicode rxvt-unicode-terminfo scrot semver slim smbclient sqlite tar sxiv subversion bzr mercurial telegram-desktop texlive-bin texlive-core texlive-humanities texlive-langextra texlive-latexextra texlive-pstricks texlive-pictures texlive-publishers texlive-formatsextra texlive-fontsextra texlive-bibtexextra texlive-science thunar thunderbird tig tk ttf-dejavu ttf-droid ttf-fira-code ttf-gentium ttf-khmer ttf-ubuntu-font-family ttf-tibetan-machine ttf-roboto ttf-opensans ttf-font-awesome ttf-fira-mono ttf-fira-sans ttf-linux-libertine traceroute udiskie unrar unzip urxvt-perls virtualbox vagrant usbutils v4l-utils vlc w3m wget whois wine wine-mono wine_gecko wireshark-cli xorg-server xorg-fonts-100dpi xorg-xkbutils xorg-xev xorg-xrandr xorg-xrdb xorg-xset xorg-xauth xorg-server-common xorg-setxkbmap xss-lock youtube-dl zathura zathura-pdf-poppler zip zsh tmux cups cups-filters foomatic-db antiword highlight chafa
         sudo pacman -Syyu --needed alacritty broot rxvt-unicode tmux compton i3-gaps neovim tig i3lock ncdu ranger curl wget rofi dmenu openbox ipython pcmanfm gcc mpv autoconf-archive pandoc htop glances iotop netcat sxiv newsboat mplayer fcitx firefox pavucontrol ncmpcpp git fzf zathura zathura-pdf-poppler gimp inkscape ack bat jq fd sed sox ctags alsa-utils python-neovim archlinux-themes-slim wqy-zenhei wqy-microhei wqy-bitmapfont network-manager-applet nm-connection-editor sshfs autoconf automake pkg-config fping ripgrep exa python-language-server bash-language-server python-rope pamixer
@@ -92,7 +96,11 @@ DOTDIR=`pwd`
 
 ROOTNAMES=("vim" "vimrc" "zshrc" "urlview" "muttrc" "urxvt" "gdbinit"  "mailcap" "signature" "signature.ru.txt" "signature.unilang" "xinitrc" "tmux.conf" "tmux-powerlinerc" "inputrc" "Xresources" "pylintrc" "pdbrc.py" "tmux")
 
-CONFIGNAMES=("alacritty" "openbox" "nvim" "ranger" "polybar" "sxiv" "i3" "ipython" "tm" "ncmpcpp.config" "vifm" "lf" "broot" "zathura")
+if [ -z "$NO_DESKTOP" ]; then
+    CONFIGNAMES=("nvim" "ranger" "ipython" "tm" "vifm" "lf" "broot")
+else
+    CONFIGNAMES=("alacritty" "openbox" "nvim" "ranger" "polybar" "sxiv" "i3" "ipython" "tm" "ncmpcpp.config" "vifm" "lf" "broot" "zathura")
+fi
 
 SCRIPTS=("suspend.sh" "linkhander" "lock.sh" "screencast.sh" "emojiselect" "wtime" "rotdir" "lf-select")
 
@@ -120,10 +128,12 @@ done
 mkdir .ncmpcpp
 ln -sfn $DOTDIR/ncmpcpp.config $HOMEDIR/.ncmpcpp/config
 ln -sfn $DOTDIR/tm $HOMEDIR/bin/tm
-ln -sfn $DOTDIR/screencast.sh $HOMEDIR/bin/screencast.sh
-ln -sfn $DOTDIR/xinitrc $HOMEDIR/.xsession
-mkdir $HOMEDIR/rofi
-ln -sfn $DOTDIR/rofi $HOMEDIR/rofi/config
+if [ -z "$NO_DESKTOP" ]; then
+    ln -sfn $DOTDIR/screencast.sh $HOMEDIR/bin/screencast.sh
+    ln -sfn $DOTDIR/xinitrc $HOMEDIR/.xsession
+    mkdir $HOMEDIR/rofi
+    ln -sfn $DOTDIR/rofi $HOMEDIR/rofi/config
+fi
 cd $HOMEDIR
 
 $DOTDIR/cache-dirs.sh
@@ -160,17 +170,19 @@ set -g @plugin 'Morantron/tmux-fingers'
 # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
 run -b '~/.tmux/plugins/tpm/tpm'
 
-cd $DOTDIR
-if [ ! -e st ]; then
-    git clone https://github.com/proycon/st
-    make && cp st ~/bin/
+if [ -z "$NO_DESKTOP" ]; then
+    cd $DOTDIR
+    if [ ! -e st ]; then
+        git clone https://github.com/proycon/st
+        make && cp st ~/bin/
+    fi
+    if [ ! -e dwm ]; then
+        git clone https://github.com/proycon/dwm
+        make && cp dwm ~/bin/
+    fi
+    if [ ! -e slstatus ]; then
+        git clone https://github.com/proycon/slstatus
+        make && cp slstatus ~/bin/
+    fi
+    cd -
 fi
-if [ ! -e dwm ]; then
-    git clone https://github.com/proycon/dwm
-    make && cp dwm ~/bin/
-fi
-if [ ! -e slstatus ]; then
-    git clone https://github.com/proycon/slstatus
-    make && cp slstatus ~/bin/
-fi
-cd -
