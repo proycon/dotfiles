e05ad589148471b83d72afe9c8e91627b93faa6a
Maarten van Gompel <proycon@anaproy.nl>
2020-07-05 00:42:05 +0200

    fixed

--- notifyclient_autoconnect.sh
+++ notifyclient_autoconnect.sh
@@ -3,9 +3,11 @@ while true; do
     fping -c1 -t5000 anaproy.nl > /dev/null 2> /dev/null
     if [ "$?" -eq "0" ]; then
         echo "starting client"
-        play ~/dotfiles/media/notifyconnect.wav
-        ~/dotfiles/notifyclient.sh
-        play ~/dotfiles/media/error.wav
+        mpv --no-video --really-quiet ~/dotfiles/media/notifyconnect.wav
+
+        ~/dotfiles/notifyclient.sh > ~/.notifications.log 2> ~/.notifications.err
+        echo  "client"
+        mpv --no-video --really-quiet ~/dotfiles/media/error.wav
     else
         echo "no network"
         sleep 5
