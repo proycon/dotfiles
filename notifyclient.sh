8589db7e578849bf9543ec710840dceb63836d3c
Maarten van Gompel <proycon@anaproy.nl>
2020-07-05 00:17:52 +0200

    configurable mqtt host

--- notifyclient.sh
+++ notifyclient.sh
@@ -18,4 +18,8 @@ if [ -z "$HOST" ]; then
     HOST=$(hostname);
 fi
 
-mosquitto_sub -I $HOST -h anaproy.nl -p 8883 -u "$MQTT_USER" -P "$MQTT_PASSWORD" --cafile /etc/ssl/certs/DST_Root_CA_X3.pem --remove-retained -t '#' -F "@H:@M:@S|%t|%p" | ~/dotfiles/notifyhandler.sh
+if [ -z "$MQTT_HOST" ]; then
+    MQTT_HOST="anaproy.nl"
+fi
+
+mosquitto_sub -I $HOST -h $MQTT_HOST -p 8883 -u "$MQTT_USER" -P "$MQTT_PASSWORD" --cafile /etc/ssl/certs/DST_Root_CA_X3.pem --remove-retained -t '#' -F "@H:@M:@S|%t|%p" $MQTT_OPTIONS | ~/dotfiles/notifyhandler.sh
