24724e06c1cc7db38eb23118552612ae7e922999
Maarten van Gompel <proycon@anaproy.nl>
2020-07-27 18:09:58 +0200

    configuring SXMO_RTCWAKEINTERVAL and rtcwake script

--- notifyclient_autoconnect.sh
+++ notifyclient_autoconnect.sh
@@ -2,12 +2,16 @@
 while true; do
     fping -c1 -t5000 anaproy.nl > /dev/null 2> /dev/null
     if [ "$?" -eq "0" ]; then
-        echo "starting client"
-        mpv --no-video --really-quiet ~/dotfiles/media/notifyconnect.wav
+        echo "starting notify client"
+        if [ ! -f /tmp/silentreconnect ]; then
+            mpv --no-video --really-quiet ~/dotfiles/media/notifyconnect.wav
+        fi
 
         ~/dotfiles/notifyclient.sh > ~/.notifications.log 2> ~/.notifications.err
-        echo  "client"
-        mpv --no-video --really-quiet ~/dotfiles/media/error.wav
+        echo  "notify client ended"
+        if [ ! -f /tmp/silentreconnect ]; then
+            mpv --no-video --really-quiet ~/dotfiles/media/error.wav
+        fi
     else
         echo "no network"
         sleep 5
