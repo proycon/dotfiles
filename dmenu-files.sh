20b9e07af93bc0be6da8ec879dbda935d3acb39b
Maarten van Gompel <proycon@anaproy.nl>
2021-04-02 00:00:06 +0200

    fix

--- dmenu-files.sh
+++ dmenu-files.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 PICKED=$(ls $2 "$1" | dmenu -l 20)
 if [ -n "$PICKED" ]; then
-    if [ -d "$1/PICKED" ]; then
-        ~/dotfiles/dmenu-files.sh "$1/$PICKED"
+    if [ -d "$1/$PICKED" ]; then
+        ~/dotfiles/dmenu-files.sh "$1/$PICKED" $2
     else
         xdg-open "$1/$PICKED" &
     fi
