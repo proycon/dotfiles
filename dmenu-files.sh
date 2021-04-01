e50f1c45e8b215b03f14f5092a74810307fae95b
Maarten van Gompel <proycon@anaproy.nl>
2021-04-02 00:41:42 +0200

    update

--- dmenu-files.sh
+++ dmenu-files.sh
@@ -4,8 +4,10 @@ if [ -n "$PICKED" ]; then
     if [ -d "$1/$PICKED" ]; then
         ~/dotfiles/dmenu-files.sh "$1/$PICKED" $2
     else
-        if file --mime-type "$1/$PICKED" | grep "text/"; then
+        if file --mime-type "$1/$PICKED" | grep -E "(text/|message/|application/xml)"; then
             alacritty -e vim "$1/$PICKED" &
+        elif file --mime-type "$1/$PICKED" | grep -E "application/pdf"; then
+            zathura "$1/$PICKED" &
         else
             xdg-open "$1/$PICKED" &
         fi
