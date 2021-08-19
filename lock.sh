672bf200da06606bcc50018ad4bf2a1a7a165a17
Maarten van Gompel <proycon@anaproy.nl>
2021-08-19 09:58:50 +0200

    status and lock fix

--- lock.sh
+++ lock.sh
@@ -1,13 +1,14 @@
-#scrot -e 'convert -resize 20% -fill "#282828" -colorize 50% -blur 0x1 -resize 500% $f ~/lockbg.png'
-if [[ ! -f /tmp/locked ]]; then
+#!/usr/bin/env sh
+if [ ! -f /tmp/locked ]; then
     lasttask=$(tail -n 1 ~/.timetracker.log | cut --delimiter=" " -f 4 | tr -d '\n')
     ~/dotfiles/timetracker.sh 0 afk
     touch /tmp/locked
     play ~/dotfiles/media/lock.wav >/dev/null 2>/dev/null &!
-    setxkbmap proylatin
     if [ -n "$WAYLAND_DISPLAY" ]; then
+        swaymsg input type:keyboard xkb_layout proylatin
         swaylock -f -c 220000
     else
+        setxkbmap proylatin
         i3lock -n -c '#220000'
     fi
     play ~/dotfiles/media/unlock.wav >/dev/null 2>/dev/null &!
