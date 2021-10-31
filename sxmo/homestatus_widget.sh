27795b598f094abd1554f96ce956fcc82eafdf21
Maarten van Gompel <proycon@anaproy.nl>
2021-10-31 17:30:00 +0100

    verbosity increase

--- sxmo/homestatus_widget.sh
+++ sxmo/homestatus_widget.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/dotfiles/homestatus.sh pango loop | wayout --font "Monospace 20" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
+~/dotfiles/homestatus.sh pango loop | wayout -vvv --font "Monospace 20" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
