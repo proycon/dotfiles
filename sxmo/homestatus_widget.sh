754a15029bf6306da68701ef24c6632dfe5f04b6
Maarten van Gompel <proycon@anaproy.nl>
2021-10-31 17:25:35 +0100

    better update of home status widget

--- sxmo/homestatus_widget.sh
+++ sxmo/homestatus_widget.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/dotfiles/homestatus.sh pango loop | wayout --font "Monospace 20" --feed-par --height 500 $1
+~/dotfiles/homestatus.sh pango loop | wayout --font "Monospace 20" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
