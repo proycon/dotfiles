4d1bc79d770fdcf19fe35405383321d95d1c6651
Maarten van Gompel <proycon@anaproy.nl>
2021-10-31 17:31:37 +0100

    fix

--- sxmo/homestatus_widget.sh
+++ sxmo/homestatus_widget.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/dotfiles/homestatus.sh pango loop | wayout -vvv --font "Monospace 20" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
+~/dotfiles/homestatus.sh pango loop | wayout -v -v -v --font "Monospace 20" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
