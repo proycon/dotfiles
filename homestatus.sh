3fa60e7e3e0c6083245f5244e1b3c5235362c276
Maarten van Gompel <proycon@anaproy.nl>
2021-10-31 12:29:19 +0100

    fix

--- homestatus.sh
+++ homestatus.sh
@@ -59,7 +59,7 @@ while [ 1 ]; do
     else
         echo "─────────────────────────────────"
     fi
-    nmcli -w 3 -c no -p -f DEVICE,STATE,NAME,TYPE con show | grep activated 2> /dev/null
+    nmcli -w 3 -c no -p -f DEVICE,STATE,NAME,TYPE con show | grep activated | sed 's/activated//' 2> /dev/null
     if [ -z "$?" ]; then
         if [ "$1" = "html" ]; then
             echo "<hr/>"
