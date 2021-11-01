6ca64464c9f1a349576e6d91bfd64c9f8c3ae5f5
Maarten van Gompel <proycon@anaproy.nl>
2021-11-01 12:57:54 +0100

    start notif client if not running

--- sxmo/homestatus_widget.sh
+++ sxmo/homestatus_widget.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env sh
+pgrep -f notifyclient.sh || ~/dotfiles/notifyclient.sh &
 ~/dotfiles/homestatus.sh pango loop | wayout --font "Monospace 20" --feed-par --height 500 $1
