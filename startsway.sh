70e3391e12003f66315b4087535049cd1b08980f
Maarten van Gompel <proycon@anaproy.nl>
2021-11-13 20:11:46 +0100

    added

--- startsway.sh
+++ startsway.sh
@@ -4,8 +4,7 @@ export GDK_BACKEND=wayland
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
 if [ "$(hostname)" = "mhysa" ]; then
-    export MOZ_WEBRENDER=0 #may be needed for wlroots-eglstream on proprietary nvidia
-    exec sway --my-next-gpu-wont-be-nvidia
+    exec sway --unsupported-gpu
 else
     exec sway
 fi
