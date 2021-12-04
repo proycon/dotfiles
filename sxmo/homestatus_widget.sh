dece47680adc5aad8ae514b305b64efc1d36c7cd
Maarten van Gompel <proycon@anaproy.nl>
2021-12-04 14:37:19 +0100

    fix

--- sxmo/homestatus_widget.sh
+++ sxmo/homestatus_widget.sh
@@ -6,4 +6,4 @@ else
     FONTSIZE="20"
 fi
 pgrep -f notifyclient.sh || ~/dotfiles/notifyclient.sh &
-~/dotfiles/homestatus.sh pango loop | wayout --fgcolor "#ffffff" --font "Monospace" --fontsize "$FONTSIZE" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
+~/dotfiles/homestatus.sh pango loop | wayout --foreground-color "#ffffff" --font "Monospace" --fontsize "$FONTSIZE" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
