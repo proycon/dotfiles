cde7db6ef977f34dfded8467c92f0f45f9531350
Maarten van Gompel <proycon@anaproy.nl>
2021-12-04 14:08:08 +0100

    adapted to new wayout

--- sxmo/homestatus_widget.sh
+++ sxmo/homestatus_widget.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 pgrep -f notifyclient.sh || ~/dotfiles/notifyclient.sh &
-~/dotfiles/homestatus.sh pango loop | wayout -v -v -v --font "Monospace 20" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
+~/dotfiles/homestatus.sh pango loop | wayout --fgcolor "#ffffff" --font "Monospace" --fontsize 20 --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
