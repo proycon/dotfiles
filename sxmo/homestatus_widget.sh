a45c8c76e1837db5082e56f93f4dcda4ac5678fd
Maarten van Gompel <proycon@anaproy.nl>
2021-12-04 14:21:36 +0100

    fontsize update

--- sxmo/homestatus_widget.sh
+++ sxmo/homestatus_widget.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
+. /etc/os-release
 pgrep -f notifyclient.sh || ~/dotfiles/notifyclient.sh &
-~/dotfiles/homestatus.sh pango loop | wayout --fgcolor "#ffffff" --font "Monospace" --fontsize 20 --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
+if [ "$DISTRIB_ID" = "postmarketos" ]; then
+    FONTSIZE="12"
+else
+    FONTSIZE="20"
+fi
+~/dotfiles/homestatus.sh pango loop | wayout --fgcolor "#ffffff" --font "Monospace" --fontsize "$FONTSIZE" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
