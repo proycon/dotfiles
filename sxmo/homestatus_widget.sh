f217ae9b0d97726515ad6ef08a0e1b37d893dd77
Maarten van Gompel <proycon@anaproy.nl>
2021-12-04 14:23:29 +0100

    fix

--- sxmo/homestatus_widget.sh
+++ sxmo/homestatus_widget.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 . /etc/os-release
-pgrep -f notifyclient.sh || ~/dotfiles/notifyclient.sh &
-if [ "$DISTRIB_ID" = "postmarketos" ]; then
+if [ "$ID" = "postmarketos" ]; then
     FONTSIZE="12"
 else
     FONTSIZE="20"
 fi
+pgrep -f notifyclient.sh || ~/dotfiles/notifyclient.sh &
 ~/dotfiles/homestatus.sh pango loop | wayout --fgcolor "#ffffff" --font "Monospace" --fontsize "$FONTSIZE" --feed-par --height 500 $1 >&2 2> /tmp/wayout.log
