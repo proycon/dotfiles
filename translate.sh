cebb46492e0eff76ed2e6a84f214e2271fff831f
Maarten van Gompel <proycon@anaproy.nl>
2022-04-16 23:06:34 +0200

    adding translation script

---
+++ translate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -n "$DISPLAY" ]; then
+    TEXT=$(xclip -out)
+fi
+
+LANG=$(~/.cargo/bin/lingua-cli "$TEXT" | cut -d " " -f 1)
+. ~/local/argostranslate.env/bin/activate
+if [ -n "$LANG" ]; then
+    TRANS=$(argos-translate -f $LANG -t en "$TEXT")
+    notify-send "Translation ($LANG)" "$TRANS"
+    echo $TRANS
+fi
