2b5b765ff60629b87492cbdf0355b028b8d5c7f9
Maarten van Gompel <proycon@anaproy.nl>
2022-07-28 22:46:34 +0200

    inbox2archive update

--- inbox2archive.sh
+++ inbox2archive.sh
@@ -1,14 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 #move everything that does not have tag inbox but is in the inbox to Archive
 if [ -f /tmp/.inbox2archive ]; then
-    echo "inbox2archive already in progress..."
+    echo "inbox2archive already in progress..."&2
 else
     touch /tmp/.inbox2archive
-    notmuch tag -inbox 'tag:inbox AND tag:killed'; #things that are killed may not remain in inbox
-    notmuch search --output=files 'not tag:inbox' | grep --color=never 'Maildir/cur' >> /tmp/.inbox2archive
-    if [ -s /tmp/.inbox2archive ]; then
-        cat /tmp/.inbox2archive | xargs mv -t /home/proycon/Maildir/.Archive/cur/
+    echo "Ensure all spam and killed messages are in the junk folder (may take a while!)">&2
+    notmuch search --output=files 'tag:killed OR tag:spam' | grep --color=never -v 'Maildir/.Junk' > /tmp/.inbox2archive #ALL things that are spam must go to spam folder
+    if [ $? -eq 0 ] && [ -s /tmp/.inbox2archive ]; then
+        cat /tmp/.inbox2archive | xargs mv -vt /home/proycon/Maildir/.Junk/cur/
     fi
-    echo "" > /tmp/.inbox2archive
+
+    #undo the inbox tag for things that are killed or spam
+    notmuch tag -inbox 'tag:inbox AND (tag:killed OR tag:spam)' 
+
+    echo "Moving everything to archive that doesn't belong in inbox folder anymore (may take a while!)">&2
+    notmuch search --output=files 'not tag:inbox' | grep --color=never 'Maildir/cur' > /tmp/.inbox2archive
+    if [ $? -eq 0 ] && [ -s /tmp/.inbox2archive ]; then
+        cat /tmp/.inbox2archive | xargs mv -vt /home/proycon/Maildir/.Archive/cur/
+    fi
     rm /tmp/.inbox2archive
 fi
