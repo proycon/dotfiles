aee32f18293299f48e29c59d924869ed3db26da9
Maarten van Gompel <proycon@anaproy.nl>
2022-07-30 14:01:59 +0200

    dark/light mode tweaks for terminals

--- lightmode.sh
+++ lightmode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ln -sf ~/.config/foot/foot.light.ini ~/.config/foot/foot.ini
-ln -sf ~/.config/alacritty/alacritty.light.yml ~/.config/alacritty.yml
+ln -sf ~/.config/alacritty/alacritty.light.yml ~/.config/alacritty/alacritty.yml
 touch ~/.light
 notify-send "light mode"
