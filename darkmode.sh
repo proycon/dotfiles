897a3b28ce949f2b160a005e550d136b78e86cd9
Maarten van Gompel <proycon@anaproy.nl>
2022-07-30 14:58:04 +0200

    fix

--- darkmode.sh
+++ darkmode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ln -sf ~/.config/foot/foot.dark.ini ~/.config/foot/foot.ini
-ln -sf ~/.config/alacritty/alacritty.dark.yml ~/.config/alacritty.yml
+ln -sf ~/.config/alacritty/alacritty.dark.yml ~/.config/alacritty/alacritty.yml
 rm ~/.light 2>/dev/null
 notify-send "dark mode"
