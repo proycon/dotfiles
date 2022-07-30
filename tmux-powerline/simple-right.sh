93195b871212551c46f2cee6b93301276b284d57
Maarten van Gompel <proycon@anaproy.nl>
2022-07-30 14:54:48 +0200

    updated

--- tmux-powerline/simple-right.sh
+++ tmux-powerline/simple-right.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-if [ -e ~/.light ]; then
-    uptime | awk -F " " '{  print "#[fg=colour132,bg=colour231] ",$6,"#[default, bg=colour231] #[fg=colour167, bg=color231]",$10,$11,$12,"#[default, bg=colour231] #[fg=colour136, bg=colour231]",$1,"#[default]" }'
-else
-    uptime | awk -F " " '{  print "#[fg=colour132,bg=colour235] ",$6,"#[default, bg=colour235] #[fg=colour167, bg=colour235]",$10,$11,$12,"#[default, bg=colour235] #[fg=colour136, bg=colour235]",$1,"#[default]" }'
-fi
+uptime | awk -F " " '{  print "#[fg=colour132,bg=colour239] ",$6,"#[default, bg=colour239] #[fg=colour139, bg=colour239]",$10,$11,$12,"#[default, bg=colour239] #[fg=normal, bg=colour239]",$1,"#[default]" }'
