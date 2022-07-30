bd68fe0d6a45c0989043177ea8fa580044fa5883
Maarten van Gompel <proycon@anaproy.nl>
2022-07-30 15:26:27 +0200

    tmux: theming

--- tmux-powerline/simple-right.sh
+++ tmux-powerline/simple-right.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-uptime | awk -F " " '{  print "#[fg=colour132,bg=colour239] ",$6,"#[default, bg=colour239] #[fg=colour139, bg=colour239]",$10,$11,$12,"#[default, bg=colour239] #[fg=normal, bg=colour239]",$1,"#[default]" }'
+NOW=$(date +%H:%M:%S)
+cat /proc/loadavg | awk -v "NOW=$NOW" -F " " '{  print "#[default, bg=colour235] #[fg=colour139, bg=colour235]",$1,"#[default, bg=colour235] #[fg=normal, bg=colour235]",NOW,"#[default]" }'
