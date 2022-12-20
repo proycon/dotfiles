688606d674e4eb5f4f567db1a10b1f3bde80ca8d
Maarten van Gompel <proycon@anaproy.nl>
2022-12-20 21:50:45 +0100

    added killmenu script

---
+++ killmenu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ~/dotfiles/colorargs.sh 
+
+CHOICE="$(
+    printf %b "
+        Cancel
+        $(ps -eo pid,tt,user,cmd | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | grep "$USER" | tail -n +2)
+    " |
+    awk '{$1=$1};1' | grep '\w' | bemenu -p 'Kill' -l 20 --fn "Monospace 16" $BEMENU_COLORARGS
+)"
+
+PID="$(echo "$CHOICE" | cut -d " " -f 1)"
+if [ -n "$PID" ] && [ "$PID" != "cancel" ]; then
+    kill "$PID"
+fi
