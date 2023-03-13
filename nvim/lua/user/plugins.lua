6a9838ce880fb990e96e5a948fa933c39070ee71
Maarten van Gompel <proycon@anaproy.nl>
2023-03-13 23:32:50 +0100

    nvim: added vim-ghost to connect with textareas in the browser using https://addons.mozilla.org/en-US/firefox/addon/ghosttext/

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -157,6 +157,9 @@ return packer.startup(function(use)
   -- use "simrat39/rust-tools.nvim" -- Rust, used for inlay hints and more
   -- use 'mfussenegger/nvim-dap' -- Debugger
   use "proycon/todo.txt-vim" -- my own extended todo.txt syntax highlighting
+  use { "raghur/vim-ghost",
+        run = ":GhostInstall" 
+  }
 
 
   -- Automatically set up your configuration after cloning packer.nvim
