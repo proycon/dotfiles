f16259d909b1e74d86e9d6d9d1b814dc8e51ba73
Maarten van Gompel <proycon@anaproy.nl>
2023-04-14 10:51:32 +0200

    nvim fix after upgrade

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -25,12 +25,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
