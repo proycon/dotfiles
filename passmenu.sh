05fcf21e7262f7091b126df4004953aa83be49b0
Maarten van Gompel <proycon@anaproy.nl>
2023-04-22 15:44:25 +0200

    added passmenu, adapted from https://git.zx2c4.com/password-store/tree/contrib/dmenu/passmenu

---
+++ passmenu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+. ~/dotfiles/colorargs.sh
+
+shopt -s nullglob globstar
+
+typeit=0
+if [ $1 == "--type" ]; then
+	typeit=1
+	shift
+fi
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+password_files=( "$prefix"/**/*.gpg )
+password_files=( "${password_files[@]#"$prefix"/}" )
+password_files=( "${password_files[@]%.gpg}" )
+
+password=$(printf '%s\n' "${password_files[@]}" | bemenu -p "Target" --fn "$BEMENU_FONT" -l 10 $BEMENU_COLORARGS )
+
+[ -n "$password" ] || exit
+
+if [ $typeit -eq 0 ]; then
+	pass show -c "$password" 2>/dev/null
+else
+	pass show "$password" | { IFS= read -r pass; printf %s "$pass"; } | ydotool type --file -
+fi
+
