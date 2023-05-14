2ad780eb6ca80e99978a1c3a9a663740474cab82
Maarten van Gompel <proycon@anaproy.nl>
2023-05-14 14:47:05 +0200

    fix

--- scripts/darkmode.sh
+++ scripts/darkmode.sh
@@ -4,5 +4,5 @@ ln -sf ~/.config/foot/foot.dark.ini ~/.config/foot/foot.ini
 ln -sf ~/.config/alacritty/alacritty.dark.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.config/rofi/rofi.dark.rasi ~/.config/rofi/config.rasi
 rm ~/.light 2>/dev/null
-swaybg -i ~/dotfiles/media/forest.jpg
+swaybg -i ~/dotfiles/media/forest.jpg &
 notify-send "dark mode"
