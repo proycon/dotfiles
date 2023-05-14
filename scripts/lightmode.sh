a1f6dff9b35b54a207394f859802a1dfa637c06b
Maarten van Gompel <proycon@anaproy.nl>
2023-05-14 13:03:27 +0200

    light mode: set wallpaper

--- scripts/lightmode.sh
+++ scripts/lightmode.sh
@@ -4,5 +4,5 @@ ln -sf ~/.config/foot/foot.light.ini ~/.config/foot/foot.ini
 ln -sf ~/.config/alacritty/alacritty.light.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.config/rofi/rofi.light.rasi ~/.config/rofi/config.rasi
 touch ~/.light
-feh --bg-fill ~/dotfiles/media/blossom-branch.jpg --image-bg '#2e3440'
+swaybg -i ~/dotfiles/media/blossom-branch.jpg
 notify-send "light mode"
