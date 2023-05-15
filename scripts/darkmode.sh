88071adb0552d5048961da7edc8ac7033f28033b
Maarten van Gompel <proycon@anaproy.nl>
2023-05-15 22:00:53 +0200

    fix

--- scripts/darkmode.sh
+++ scripts/darkmode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -sf ~/.config/foot/foot.dark.$HOST.ini ~/.config/foot/foot.ini
+ln -sf ~/.config/foot/foot.dark.$(hostname).ini ~/.config/foot/foot.ini
 ln -sf ~/.config/rofi/rofi.dark.rasi ~/.config/rofi/config.rasi
 rm ~/.light 2>/dev/null
 swaybg -m fill -i ~/dotfiles/media/forest.jpg &
--- scripts/lightmode.sh
+++ scripts/lightmode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -sf ~/.config/foot/foot.light.$HOST.ini ~/.config/foot/foot.ini
+ln -sf ~/.config/foot/foot.light.$(hostname).ini ~/.config/foot/foot.ini
 ln -sf ~/.config/alacritty/alacritty.light.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.config/rofi/rofi.light.rasi ~/.config/rofi/config.rasi
 touch ~/.light
