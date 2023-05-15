3e5900f18fcbb7c0451d077a2286dcb3c6c6db1b
Maarten van Gompel <proycon@anaproy.nl>
2023-05-15 20:14:59 +0200

    fixes for foot config differences accross systems

--- scripts/darkmode.sh
+++ scripts/darkmode.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-ln -sf ~/.config/foot/foot.dark.ini ~/.config/foot/foot.ini
-ln -sf ~/.config/alacritty/alacritty.dark.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/.config/foot/foot.dark.$HOST.ini ~/.config/foot/foot.ini
 ln -sf ~/.config/rofi/rofi.dark.rasi ~/.config/rofi/config.rasi
 rm ~/.light 2>/dev/null
 swaybg -m fill -i ~/dotfiles/media/forest.jpg &
