379105a94db698000c98af4b0464c42e259d20b5
Maarten van Gompel <proycon@anaproy.nl>
2023-07-07 14:37:56 +0200

    added light theme

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -72,6 +72,7 @@ return packer.startup(function(use)
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   use "NLKNguyen/papercolor-theme"
+  use "sainnhe/everforest"
   use "morhetz/gruvbox"
   use "proycon/gruvbox-baby"
   --use "luisiacc/gruvbox-baby"
