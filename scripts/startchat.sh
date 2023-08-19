e4c824f49469376928c03ec0b0eaac4734792657
Maarten van Gompel <proycon@anaproy.nl>
2023-08-19 17:38:10 +0200

    switch to senpai

--- scripts/startchat.sh
+++ scripts/startchat.sh
@@ -2,4 +2,4 @@
 pidof tg || foot --title tg tg &
 pidof tut-mastodon || foot --title tut-mastodon tut-mastodon &
 pidof gomuks || foot --title gomuks gomuks &
-pidof tiny || foot --title tiny tiny &
+pidof tiny || foot --title senpai senpai-irc &
