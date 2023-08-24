9d1235507eb6289536b9f61ab0485a16ad6448ff
Maarten van Gompel <proycon@anaproy.nl>
2023-08-24 20:10:50 +0200

    Revert "sxmo: check various wlan interfaces"
    This reverts commit 84743904427ca0631b336eeb289cb3ad006bc868.

--- sxmo/guardbwusage.sh
+++ sxmo/guardbwusage.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-if [ -e /sys/class/net/wwan0 ]; then
-    USAGE=$(vnstat -m -i wwan0 --oneline b | cut -d ';' -f 11)
-elif [ -e /sys/class/net/wlan0 ]; then
-    USAGE=$(vnstat -m -i wlan0 --oneline b | cut -d ';' -f 11)
-fi
+USAGE=$(vnstat -m -i wwan0 --oneline b | cut -d ';' -f 11)
 if [ -n "$USAGE" ]; then
     if [ $USAGE -gt 1835008000 ]; then
         nmcli connection down Simyo && mpv expired.oga
