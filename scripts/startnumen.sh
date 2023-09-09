e208f89e2f6f50a2a5fdd252d5b35510bd3d1d3f
Maarten van Gompel <proycon@anaproy.nl>
2023-09-09 22:13:48 +0200

    setting up numen again

--- scripts/startnumen.sh
+++ scripts/startnumen.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 killall speech
 paplay ~/dotfiles/media/glass.ogg
-numen
+
+if [ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]; then
+    ENVPHRASES=~/dotfiles/numen/phrases/environment.hyprland.phrases
+elif [ -n "$SXMO_WM" ]; then
+    ENVPHRASES=~/dotfiles/numen/phrases/environment.sxmo.phrases
+fi
+numen /etc/numen/phrases/character.phrases /etc/numen/phrases/control.phrases /etc/numen/phrases/voice.phrases ~/dotfiles/numen/phrases/extra.character.phrases "$ENVPHRASES" ~/dotfiles/numen/phrases/house.phrases
 paplay ~/dotfiles/media/glass_low.ogg
 
