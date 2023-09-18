bdacd0aa6e70d909245056a6b8dd10c850313f0e
Maarten van Gompel <proycon@anaproy.nl>
2023-09-18 23:13:22 +0200

    fix

--- scripts/startchat.sh
+++ scripts/startchat.sh
@@ -2,4 +2,4 @@
 pidof tg || foot --title tg tg &
 pidof tut-mastodon || foot --title tut-mastodon tut-mastodon &
 pidof gomuks || foot --title gomuks gomuks &
-pidof tiny || foot --title senpai senpai-irc &
+pidof senpai || foot --title senpai senpai-irc &
