aebbd62da94237b037d895e39e560ae8f0159820
Maarten van Gompel <proycon@anaproy.nl>
2023-12-02 15:08:38 +0100

    added comment

--- scripts/vpn-knaw.sh
+++ scripts/vpn-knaw.sh
@@ -6,3 +6,5 @@ sleep 10 && (
     ip route add 194.171.119/24 dev ppp0
 ) &
 openfortivpn -c /home/proycon/dotfiles.private/openfortivpn-knaw
+
+#openfortivpn-1.20.3-1 with ppp 2.4.9-3 works... latest versions don't!
