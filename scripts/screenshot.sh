84d917c8952d086476b8598b07e5c34653cc5af2
Maarten van Gompel <proycon@anaproy.nl>
2024-01-17 12:57:44 +0100

    region screenshot

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 if [ -n "$1" ]; then
-    grim "/home/proycon/$(date +%Y-%m-%d-%H%M%S)_screenshot.png" && notify-send "Screenshot ready"
+    NAME="screenshot$(date +%Y%m%d%H%M%S).png"
+    wl-copy "https://download.anaproy.nl/$NAME"
+    if grim -g "$(slurp)" "/nettmp/$NAME"; then
+        notify-send "Screenshot ready" "Published at https://download.anaproy.nl/$NAME and URL copied to clipboard"
+    else
+        notify-send "Screenshot failed!"
+    fi
 else
     grim "/home/proycon/$(date +%Y-%m-%d-%H%M%S)_screenshot.png" && notify-send "Screenshot ready"
 fi
