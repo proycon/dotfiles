be024102e0b104a3290e422c99afa5c683405208
Maarten van Gompel <proycon@anaproy.nl>
2024-01-17 13:11:55 +0100

    screenshotting updates

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -n "$1" ]; then
+if [ "$1" = "region" ]; then
     NAME="screenshot$(date +%Y%m%d%H%M%S).png"
     wl-copy "https://download.anaproy.nl/$NAME"
     if grim -g "$(slurp)" "/nettmp/$NAME"; then
@@ -7,7 +7,17 @@ if [ -n "$1" ]; then
     else
         notify-send "Screenshot failed!"
     fi
+elif [ "$1" = "annotate" ]; then
+    NAME="screenshot$(date +%Y%m%d%H%M%S).png"
+    wl-copy "https://download.anaproy.nl/$NAME"
+    if grim -g "$(slurp -o -r -c '#ff0000ff')" - | satty --filename - --fullscreen --output-filename "/nettmp/$NAME"; then
+        notify-send "Screenshot ready" "Published at https://download.anaproy.nl/$NAME and URL copied to clipboard"
+    else
+        notify-send "Screenshot failed!"
+    fi
 else
-    grim "/home/proycon/$(date +%Y-%m-%d-%H%M%S)_screenshot.png" && notify-send "Screenshot ready"
+    NAME="$(date +%Y-%m-%d-%H%M%S)_screenshot.png"
+    wl-copy "/home/proycon/$NAME"
+    grim "/home/proycon/$NAME" && notify-send "Full screenshot ready" "In /home/proycon/$NAME (copied to clipboard)"
 fi
 
