1c5ca70fdf86d0ff9b8f55d072c0befe959289e7
Maarten van Gompel <proycon@anaproy.nl>
2024-01-28 12:16:12 +0100

    added fractal

--- scripts/startchat.sh
+++ scripts/startchat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pidof tg || foot --title tg tg &
 pidof tut-mastodon || foot --title tut-mastodon tut-mastodon &
-pidof gomuks || foot --title gomuks gomuks &
+pidof fractal || fractal &
 pidof senpai || foot --title senpai senpai-irc &
