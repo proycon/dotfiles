119c31ad88ff39ef7a343b9b310d116466f05f25
Maarten van Gompel <proycon@anaproy.nl>
2024-02-01 23:48:28 +0100

    changed telegram client and removed tut

--- scripts/startchat.sh
+++ scripts/startchat.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-pidof tg || foot --title tg tg &
-pidof tut-mastodon || foot --title tut-mastodon tut-mastodon &
+pidof telegram-desktop || telegram-desktop &
+#pidof tg || foot --title tg tg &
+#pidof tut-mastodon || foot --title tut-mastodon tut-mastodon &
 pidof nheko || nheko &
 pidof senpai || foot --title senpai senpai-irc &
