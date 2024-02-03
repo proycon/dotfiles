4ce614d1445f0e027ab2c5f130be790ce5e97c49
Maarten van Gompel <proycon@anaproy.nl>
2024-02-03 13:58:59 +0100

    simplified tmux setup

--- tmux-powerline/simple-right.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-NOW=$(date +%H:%M:%S)
-cat /proc/loadavg | awk -v "NOW=$NOW" -F " " '{  print "#[default, bg=colour235] #[fg=colour139, bg=colour235]",$1,"#[default, bg=colour235] #[fg=normal, bg=colour235]",NOW,"#[default]" }'
