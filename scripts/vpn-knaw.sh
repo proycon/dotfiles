9744244c732e5eaa7babd965ee051691aa568f53
Maarten van Gompel <proycon@anaproy.nl>
2024-02-16 15:27:51 +0100

    fix

--- scripts/vpn-knaw.sh
+++ scripts/vpn-knaw.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-sleep 10 && (
+sleep 5 && (
     ip route add 195.169.0.0/16 dev ppp0
-    ip route add 195.171.119/24 dev ppp0
+    ip route add 195.171.119.0/24 dev ppp0
+    ip route add 194.171.119.0/24 dev ppp0
     ip route add 10.27.1.0/24 dev ppp0
-    ip route add 194.171.119/24 dev ppp0
+    echo "routes set"
 ) &
 openfortivpn -c /home/proycon/dotfiles.private/openfortivpn-knaw
 
