18d8551907a67d85d5e00c41c36c1b823decd1a0
Maarten van Gompel <proycon@anaproy.nl>
2024-06-06 22:49:20 +0200

    added VPN range

--- scripts/vpn-knaw.sh
+++ scripts/vpn-knaw.sh
@@ -3,6 +3,7 @@ sleep 5 && (
     ip route add 195.169.0.0/16 dev ppp0
     ip route add 195.171.119.0/24 dev ppp0
     ip route add 194.171.119.0/24 dev ppp0
+    ip route add 194.171.4.0/24 dev ppp0
     ip route add 10.27.1.0/24 dev ppp0
     echo "routes set"
 ) &
