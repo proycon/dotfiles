b981a673d141c3eabbf8fa5f90528f5508e4d9a0
Maarten van Gompel <proycon@anaproy.nl>
2024-07-05 14:06:43 +0200

    neovim: updated gitsigns

--- nvim/lua/user/gitsigns.lua
+++ nvim/lua/user/gitsigns.lua
@@ -5,11 +5,11 @@ end
 
 gitsigns.setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    add = {  text = "▎" },
+    change = {  text = "▎" },
+    delete = {  text = "契" },
+    topdelete = {  text = "契" },
+    changedelete = {  text = "▎"  },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -41,8 +41,5 @@ gitsigns.setup {
     relative = "cursor",
     row = 0,
     col = 1,
-  },
-  yadm = {
-    enable = false,
   },
 }
