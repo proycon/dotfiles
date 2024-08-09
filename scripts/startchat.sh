2bfff7a4ac26833d8aace550cdbe554a51301d95
Maarten van Gompel <proycon@anaproy.nl>
2024-08-09 15:06:08 +0200

    senpai fix

--- scripts/startchat.sh
+++ scripts/startchat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sleep 1
+pidof senpai || foot --title senpai senpai &
 pidof telegram-desktop || telegram-desktop &
 #pidof tg || foot --title tg tg &
 #pidof tut-mastodon || foot --title tut-mastodon tut-mastodon &
 pidof nheko || nheko &
-pidof senpai || foot --title senpai senpai-irc &
