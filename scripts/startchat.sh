d4fc6e430bbc22a71b82c5aca376bfbfb5234a5b
Maarten van Gompel <proycon@anaproy.nl>
2024-09-24 13:08:47 +0200

    added scli and dino

--- scripts/startchat.sh
+++ scripts/startchat.sh
@@ -4,4 +4,9 @@ pidof senpai || foot --title senpai senpai &
 pidof telegram-desktop || telegram-desktop &
 #pidof tg || foot --title tg tg &
 #pidof tut-mastodon || foot --title tut-mastodon tut-mastodon &
+if [ "$HOST" = "pollux" ]; then
+    pidof scli || foot --title scli scli &
+fi
+pidof dino || (GTK_THEME=Adwaita-dark dino) &
 pidof nheko || nheko &
+
