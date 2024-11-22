071a6b8b4d29f019f1615ba663902187b097f4b8
Maarten van Gompel <proycon@anaproy.nl>
2024-11-22 19:34:03 +0100

    autodetect interface

--- scripts/vpn-knaw.sh
+++ scripts/vpn-knaw.sh
@@ -2,14 +2,18 @@
 
 #sleep for a bit so we only run this when the connection is up (bit patchy but works)
 sleep 4 && (
+    IF=$(nmcli connection show --active | grep -E "(wifi|ethernet)" | head -n 1 |  tr -s " " | cut -d" " -f 4)
+    if [ -z "$IF" ]; then
+        IF=enp10s0
+    fi
     VPN_REMOTE=192.87.139.238
     ip route add 195.169.0.0/16 via $VPN_REMOTE dev ppp0
     ip route add 195.171.119.0/24 via $VPN_REMOTE dev ppp0
     ip route add 194.171.119.0/24 via $VPN_REMOTE dev ppp0
     ip route add 194.171.4.0/24 via $VPN_REMOTE dev ppp0
     ip route add 10.27.1.0/24 via $VPN_REMOTE dev ppp0
-    ip route add $VPN_REMOTE via 192.168.0.1 dev enp10s0 || route del $VPN_REMOTE #delete the existing one if this fails
-    ip route add $VPN_REMOTE via 192.168.0.1 dev enp10s0 #and retry...
+    ip route add $VPN_REMOTE via 192.168.0.1 dev "$IF" || route del $VPN_REMOTE #delete the existing one if this fails
+    ip route add $VPN_REMOTE via 192.168.0.1 dev "$IF" #and retry...
     echo "routes set"
 ) &
 
