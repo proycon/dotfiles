c0f718f107b6105066867cbf82aedde10d141494
Maarten van Gompel <proycon@anaproy.nl>
2024-11-29 14:07:59 +0100

    nvim: re-enable clangd LSP conditionally based on distro

--- nvim/lua/user/lsp/configs.lua
+++ nvim/lua/user/lsp/configs.lua
@@ -7,9 +7,28 @@ end
 
 local lspconfig = require("lspconfig")
 
+local function getLinuxDistroFromReleaseFile()
+    local f = io.open("/etc/os-release", "r")
+    if not f then return nil end
+
+    for line in f:lines() do
+        if line:match("^ID=") then
+            local distroName = line:match("[^=]+") -- Extract value after "=" sign
+            return distroName
+        end
+    end
+    f:close()
+    return nil
+end
+
 -- (proycon) I don't think it should be necessary to add all those explicitly here (kinda conflicts with nvim-lsp-installer), but I can't get it to work otherwise
 local servers = { "jsonls", "lua_ls", "pyright", 'html', 'bashls', 'rust_analyzer', 'texlab', 'yamlls', 'jsonls', 'jdtls','kotlin_language_server',  'cssls', 'eslint', 'gopls', 'ansiblels', 'zls' }
--- (proycon) these don't work on arm alpine: 'clangd', 'lemminx'
+-- (proycon) these don't work on arm alpine: 'clangd', 'lemminx', add them conditionally
+local distro = getLinuxDistroFromReleaseFile()
+if distro ~= "postmarketos" and distro ~= "alpine" then
+   table.insert(servers, "clangd")
+   table.insert(servers, "lemminx")
+end
 
 lsp_installer.setup {
 	ensure_installed = servers
