eb02486432f9db77cce52594d13c783aa15e36e0
Maarten van Gompel <proycon@anaproy.nl>
2024-12-20 13:22:26 +0100

    screenshot: add random component to screenshot names

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$1" = "region" ]; then
-    NAME="screenshot$(date +%Y%m%d%H%M%S).png"
+    NAME="screenshot$(date +%Y%m%d%H%M%S)-$(pwgen -1 -0 -A).png"
     wl-copy "https://download.anaproy.nl/$NAME"
     if grim -g "$(slurp)" "/nettmp/$NAME"; then
         ln -s "/nettmp/$NAME" "/tmp/$NAME"
@@ -9,7 +9,7 @@ if [ "$1" = "region" ]; then
         notify-send "Screenshot failed!"
     fi
 elif [ "$1" = "annotate" ]; then
-    NAME="screenshot$(date +%Y%m%d%H%M%S).png"
+    NAME="screenshot$(date +%Y%m%d%H%M%S)-$(pwgen -1 -0 -A).png"
     wl-copy "https://download.anaproy.nl/$NAME"
     if grim -g "$(slurp -o -r -c '#ff0000ff')" - | satty --filename - --fullscreen --output-filename "/nettmp/$NAME"; then
         ln -s "/nettmp/$NAME" "/tmp/$NAME"
@@ -18,7 +18,7 @@ elif [ "$1" = "annotate" ]; then
         notify-send "Screenshot failed!"
     fi
 else
-    NAME="$(date +%Y-%m-%d-%H%M%S)_screenshot.png"
+    NAME="$(date +%Y-%m-%d-%H%M%S)_screenshot-$(pwgen -1 -0 -A).png"
     wl-copy "/home/proycon/$NAME"
     grim "/home/proycon/$NAME" && notify-send "Full screenshot ready" "In /home/proycon/$NAME (copied to clipboard)"
 fi
