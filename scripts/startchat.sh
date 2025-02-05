9280e47f5bc875d5f329d45d0764a99f861457a2
Maarten van Gompel <proycon@anaproy.nl>
2025-02-05 20:10:42 +0100

    don't start telegram and scli anymore

--- scripts/startchat.sh
+++ scripts/startchat.sh
@@ -2,11 +2,8 @@
 sleep 1
 export GTK_THEME=Adwaita-dark
 pidof senpai || foot --title senpai senpai &
-pidof telegram-desktop || telegram-desktop &
+#pidof telegram-desktop || telegram-desktop &
 #pidof tg || foot --title tg tg &
 #pidof tut-mastodon || foot --title tut-mastodon tut-mastodon &
-if [ "$HOST" = "pollux" ]; then
-    pidof scli || foot --title scli scli &
-fi
 pidof dino || dino &
 pidof nheko || nheko &
