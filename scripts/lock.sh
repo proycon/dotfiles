aadad203875ed76bf20831716d7af8d36bde1b29
Maarten van Gompel <proycon@anaproy.nl>
2025-02-28 13:29:35 +0100

    prefer librewolf over firefox

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -2,7 +2,7 @@
 if [ ! -f /tmp/locked ]; then
     if pidof mpv steam X-Plane-x86_64; then
         #no lock
-        exit 0
+        exit 1
     fi
     task=$(todo.sh timetrack current -t)
     todo.sh timetrack stop
