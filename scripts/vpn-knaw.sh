7320ccf1da313b41694a1da5440e25d970f5f577
Maarten van Gompel <proycon@anaproy.nl>
2025-11-15 11:52:06 +0100

    added VPN routes

--- scripts/vpn-knaw.sh
+++ scripts/vpn-knaw.sh
@@ -12,6 +12,8 @@ sleep 4 && (
     ip route add 194.171.119.0/24 via $VPN_REMOTE dev ppp0
     ip route add 194.171.4.0/24 via $VPN_REMOTE dev ppp0
     ip route add 10.27.1.0/24 via $VPN_REMOTE dev ppp0
+    ip route add 10.26.4.0/24 via $VPN_REMOTE dev ppp0
+    ip route add 10.24.0.0/16 via $VPN_REMOTE dev ppp0
     ip route add $VPN_REMOTE via 192.168.0.1 dev "$IF" || route del $VPN_REMOTE #delete the existing one if this fails
     ip route add $VPN_REMOTE via 192.168.0.1 dev "$IF" #and retry...
     echo "routes set"
