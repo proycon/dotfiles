bd90f9393e2fc69e71552eaeb5d7af5e214ecc84
Maarten van Gompel <proycon@anaproy.nl>
2025-12-15 12:24:11 +0100

    fixed vpn routing

--- scripts/vpn-knaw.sh
+++ scripts/vpn-knaw.sh
@@ -6,16 +6,16 @@ sleep 4 && (
     if [ -z "$IF" ]; then
         IF=enp10s0
     fi
-    VPN_REMOTE=192.87.139.238
-    ip route add 195.169.0.0/16 via $VPN_REMOTE dev ppp0
-    ip route add 195.171.119.0/24 via $VPN_REMOTE dev ppp0
-    ip route add 194.171.119.0/24 via $VPN_REMOTE dev ppp0
-    ip route add 194.171.4.0/24 via $VPN_REMOTE dev ppp0
-    ip route add 10.27.1.0/24 via $VPN_REMOTE dev ppp0
-    ip route add 10.26.4.0/24 via $VPN_REMOTE dev ppp0
-    ip route add 10.24.0.0/16 via $VPN_REMOTE dev ppp0
-    ip route add $VPN_REMOTE via 192.168.0.1 dev "$IF" || route del $VPN_REMOTE #delete the existing one if this fails
-    ip route add $VPN_REMOTE via 192.168.0.1 dev "$IF" #and retry...
+    VPN_GATEWAY=$(ip --json addr show ppp0 | jq -r '.[0].addr_info[0].local')
+    ip route add 195.169.0.0/16 via $VPN_GATEWAY dev ppp0
+    ip route add 195.171.119.0/24 via $VPN_GATEWAY dev ppp0
+    ip route add 194.171.119.0/24 via $VPN_GATEWAY dev ppp0
+    ip route add 194.171.4.0/24 via $VPN_GATEWAY dev ppp0
+    ip route add 10.27.1.0/24 via $VPN_GATEWAY dev ppp0
+    ip route add 10.26.4.0/24 via $VPN_GATEWAY dev ppp0
+    ip route add 10.24.0.0/16 via $VPN_GATEWAY dev ppp0
+    ip route add $VPN_GATEWAY via 192.168.0.1 dev "$IF" || route del $VPN_GATEWAY #delete the existing one if this fails
+    ip route add $VPN_GATEWAY via 192.168.0.1 dev "$IF" #and retry...
     echo "routes set"
 ) &
 
