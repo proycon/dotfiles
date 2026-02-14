569ad7a654863efaa6dd5b35f1101bbf89b0b379
Maarten van Gompel <proycon@anaproy.nl>
2026-02-14 14:38:07 +0100

    fixup

--- sxmo/guardbwusage.sh
+++ sxmo/guardbwusage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-USAGE=$(vnstat -m -i wwan0 --oneline b 2> /dev/null | cut -d ';' -f 11)
+USAGE=$(vnstat -m -i wwan0 --oneline b | cut -d ';' -f 11) 2> /dev/null 
 if [ -n "$USAGE" ]; then
     if [ $USAGE -gt 1835008000 ]; then
         nmcli connection down Simyo && mpv expired.oga
